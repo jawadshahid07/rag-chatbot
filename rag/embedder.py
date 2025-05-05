458b719481dd6685b6837d453492c6baaa3db0cc
jawadshahid07 <jawad.shahid70@outlook.com>
2025-05-05 23:17:06 +0500

    convert data into vector embeddings

---
+++ rag/embedder.py
@@ -0,0 +1,9 @@
+# rag/embedder.py
+
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+def create_vectorstore(chunks):
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    vectorstore = FAISS.from_documents(chunks, embeddings)
+    return vectorstore
