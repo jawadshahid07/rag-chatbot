1f4673a8bc9eaaf32c3d3ecb841ffb8f1051bfeb
jawadshahid07 <jawad.shahid70@outlook.com>
2025-05-06 13:34:30 +0500

    add unstructured data to data store

--- rag/data_loader.py
+++ rag/data_loader.py
@@ -1,6 +1,8 @@
 import json
 from langchain_core.documents import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+import os
 
 def load_json_documents(file_path):
     with open(file_path, "r", encoding="utf-8") as f:
@@ -19,3 +21,13 @@ def load_json_documents(file_path):
 def chunk_documents(documents, chunk_size=500, chunk_overlap=50):
     splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     return splitter.split_documents(documents)
+
+def load_pdfs_from_folder(folder_path):
+    all_docs = []
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".pdf"):
+            full_path = os.path.join(folder_path, filename)
+            loader = PyPDFLoader(full_path)
+            pdf_docs = loader.load()
+            all_docs.extend(pdf_docs)
+    return all_docs
